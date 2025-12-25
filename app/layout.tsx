a6d86b83cb935383e90cd6d883001205d0b45719
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-26 00:07:54 +0200

    wrapping project content with theme provider and setting required attributes/props (html element)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/assets/styles/globals.css";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
+import { ThemeProvider } from "next-themes";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -24,8 +25,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} antialiased`}>{children}</body>
+    // suppressHydrationWarning => needed cus next-themes uses the window object and the server comp doesnt have a windo object 
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          //disable all css transitions when switching themes to avoid issues
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
