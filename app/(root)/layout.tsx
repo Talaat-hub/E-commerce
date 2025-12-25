c20a5982812942fe7c6f37e5cd4deb231d429851
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-25 19:30:40 +0200

    updating root latout file

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,3 @@
-import type { Metadata } from "next";
-// import "@/assets/styles/globals.css";
-
-export const metadata: Metadata = {
-  title: "Store",
-  description: "A modern ecommerce platform build with Next.js",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
