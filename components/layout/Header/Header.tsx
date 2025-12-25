be3667babd8f2a1536e566164609c45d323ef542
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-25 20:25:13 +0200

    creating the header's logo and links (Navbar)

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,5 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/images/logo.svg";
+import { APP_NAME } from "@/lib/constants";
+import { Button } from "@/components/ui/button";
+import { ShoppingCart, UserIcon } from "lucide-react";
+
+console.log(Logo);
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link href="/" className="flex-start">
+            <Image
+              src={Logo}
+              alt={`${APP_NAME} logo`}
+              width={48}
+              height={48}
+              priority
+            />
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+        <div className="space-x-2">
+          <Button asChild variant="ghost">
+            <Link href={"/cart"}>
+              <ShoppingCart /> cart
+            </Link>
+          </Button>
+          <Button asChild variant="ghost">
+            <Link href={"/sign-in"}>
+              <UserIcon /> Sign In
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
