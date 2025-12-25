764a74bf96162c3747ad4de1e6a4ec39087bba91
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-25 21:12:44 +0200

    updating footer

--- components/layout/Footer/Footer.tsx
+++ components/layout/Footer/Footer.tsx
@@ -4,22 +4,8 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="border-t">
-      <div className="p-5 flex-center flex-col">
-        <div className="flex my-2">
-          <p className="mr-2">
-            made with ❤️ by
-            <a
-              className="font-bold hover:text-primary transition-primary ml-1"
-              href="https://github.com/MohammedNaderAlGhussin"
-              target="_blank"
-            >
-              Mohammednader AlGhussin
-            </a>
-          </p>
-        </div>
-        <div>
-          © {currentYear} {APP_NAME}. All Rights Reserved.
-        </div>
+      <div className="p-5 flex-center">
+        © {currentYear} {APP_NAME}. All Rights Reserved.
       </div>
     </footer>
   );
