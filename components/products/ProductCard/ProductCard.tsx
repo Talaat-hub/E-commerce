8a2e5ee5c06dd3d557fb1d86585743f83da5ad69
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-26 18:31:37 +0200

    modify styling and small screens appearance

--- components/products/ProductCard/ProductCard.tsx
+++ components/products/ProductCard/ProductCard.tsx
@@ -16,6 +16,7 @@ const ProductCard = ({ product }: ProductCardProps) => {
             height={300}
             width={300}
             priority={true}
+            className="w-fit"
           />
         </Link>
       </CardHeader>
