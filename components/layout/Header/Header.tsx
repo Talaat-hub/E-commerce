09983e16b2fcea5e71bde4808b7c88a849dc89cf
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-27 16:32:13 +0200

    updating logo and logo text place

--- components/layout/Header/Header.tsx
+++ components/layout/Header/Header.tsx
@@ -1,12 +1,19 @@
+import { APP_NAME } from "@/lib/constants";
 import HeaderMenu from "./HeaderMenu";
 import Logo from "./Logo";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex-between">
         <div className="flex-start">
-          <Logo />
+          <Link href="/" className="flex-start">
+            <Logo />
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
+          </Link>
         </div>
         <HeaderMenu />
       </div>
--- components/layout/Header/Logo.tsx
+++ components/layout/Header/Logo.tsx
@@ -6,7 +6,6 @@ import Light from "@/public/images/kaufra-light.png";
 import { useTheme } from "next-themes";
 import { useMounted } from "@/hooks/useMounted";
 import { APP_NAME } from "@/lib/constants";
-import Link from "next/link";
 
 const Logo = () => {
   const { resolvedTheme } = useTheme();
@@ -15,18 +14,13 @@ const Logo = () => {
   if (!mounted) return null;
 
   return (
-    <Link href="/" className="flex-start">
-      <Image
-        src={resolvedTheme === "dark" ? Light : Dark}
-        alt={`${APP_NAME} logo`}
-        width={60}
-        height={50}
-        priority
-      />
-      <span className="hidden lg:block font-bold text-2xl ml-3">
-        {APP_NAME}
-      </span>
-    </Link>
+    <Image
+      src={resolvedTheme === "dark" ? Light : Dark}
+      alt={`${APP_NAME} logo`}
+      width={60}
+      height={50}
+      priority
+    />
   );
 };
 
