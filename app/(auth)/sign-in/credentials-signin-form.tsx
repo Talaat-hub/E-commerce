45420e0afe541d028c88edfb385802b0fb14a386
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-28 22:20:23 +0200

    creating a sign in form

---
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { signInDefaultValues } from "@/lib/constants";
+import Link from "next/link";
+
+const CredentialsSignInForm = () => {
+  return (
+    <form>
+      <div className="space-y-6">
+        <div className="flex flex-col gap-3">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            required
+            autoComplete="email"
+            defaultValue={signInDefaultValues.email}
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <Label htmlFor="password">Password</Label>
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            required
+            autoComplete="password"
+            defaultValue={signInDefaultValues.password}
+          />
+        </div>
+        <Button className="w-full cursor-pointer" variant="default">
+          Sign In
+        </Button>
+        <div className="text-sm text-center text-muted-foreground">
+          Don&apos;t have an account?{" "}
+          <Link
+            href="/sign-up"
+            target="_self"
+            className="link hover:text-blue-400 duration-300"
+          >
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CredentialsSignInForm;
