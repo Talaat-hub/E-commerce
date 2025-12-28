464cef79c59acf705f788d3e28223f593a2ab87f
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-29 00:53:46 +0200

    handling sign in, invalid credential and disabling sign in button

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -3,12 +3,34 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
 import { signInDefaultValues } from "@/lib/constants";
+import { SignInFormState } from "@/types/signInFormState";
 import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 
 const CredentialsSignInForm = () => {
+  const [state, formAction] = useActionState<SignInFormState, FormData>(
+    signInWithCredentials,
+    {
+      success: false,
+      message: "",
+    }
+  );
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button disabled={pending} className="w-full" variant="default">
+        {pending ? "Signing In..." : "Sign In"}
+      </Button>
+    );
+  };
+
   return (
-    <form>
+    <form action={formAction}>
       <div className="space-y-6">
         <div className="flex flex-col gap-3">
           <Label htmlFor="email">Email</Label>
@@ -32,9 +54,10 @@ const CredentialsSignInForm = () => {
             defaultValue={signInDefaultValues.password}
           />
         </div>
-        <Button className="w-full cursor-pointer" variant="default">
-          Sign In
-        </Button>
+        <SignInButton />
+        {state && !state.success && (
+          <div className="text-center text-destructive ">{state.message}</div>
+        )}
         <div className="text-sm text-center text-muted-foreground">
           Don&apos;t have an account?{" "}
           <Link
