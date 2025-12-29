0792b4aa50729e53f0461297f7a9a7c712f6b6fd
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-29 04:28:11 +0200

    created a format error msg to display error msgs

--- lib/utils.ts
+++ lib/utils.ts
@@ -16,3 +16,34 @@ export function formatNumberWithDecimal(num: number): string {
   const [int, decimal] = num.toString().split(".");
   return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
+
+import { ZodError } from "zod";
+import { Prisma } from "@prisma/client";
+
+//Format Errors
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function formatError(error: any) {
+  // Zod
+  if (error instanceof ZodError) {
+    return error.issues.map((issue) => issue.message).join(". ");
+  }
+
+  // Prisma unique constraint
+  if (
+    error instanceof Prisma.PrismaClientKnownRequestError &&
+    error.code === "P2002"
+  ) {
+    const field = Array.isArray(error.meta?.target)
+      ? error.meta.target[0]
+      : "Email";
+
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`;
+  }
+
+  //  Fallback
+  if (typeof error?.message === "string") {
+    return error.message;
+  }
+
+  return "Something went wrong. Please try again.";
+}
