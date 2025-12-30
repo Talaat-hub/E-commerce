e65ecb6e88024e6aec4e11c9d38c4d6dae9d965a
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-30 08:53:55 +0200

    creating the products table and displaying them in it

--- app/(root)/cart/CartTable.tsx
+++ app/(root)/cart/CartTable.tsx
@@ -1,12 +1,23 @@
-import { Cart } from "@/types/cart";
+"use client";
+
+import { Cart, CartItem } from "@/types/cart";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { useTransition } from "react";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCell,
+} from "@/components/ui/table";
+import Image from "next/image";
+import { Loader, Minus, Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { toast } from "sonner";
 
 const CartTable = ({ cart }: { cart?: Cart }) => {
-  const router = useRouter();
-  const [isPending, startTransition] = useTransition();
-
   return (
     <>
       <h1 className="py-4 h2-bold">Shopping Cart</h1>
@@ -16,11 +27,99 @@ const CartTable = ({ cart }: { cart?: Cart }) => {
         </div>
       ) : (
         <div className="grid md:grid-cols-4 md:gap-5">
-          <div className="overflow-x-auto md:col-span-3">Table</div>
+          <div className="overflow-x-auto md:col-span-3">
+            {" "}
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Item</TableHead>
+                  <TableHead className="text-center">Quantity</TableHead>
+                  <TableHead className="text-right">Price</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cart.items.map((item) => (
+                  <TableRow key={item.slug}>
+                    <TableCell>
+                      <Link
+                        href={`/product/${item.slug}`}
+                        className="flex items-center"
+                      >
+                        <Image
+                          src={item.image}
+                          alt={item.name}
+                          width={50}
+                          height={50}
+                        />
+                        <span className="px-2">{item.name}</span>
+                      </Link>
+                    </TableCell>
+                    <TableCell className="flex-center gap-2 mt-3">
+                      <RemoveButton item={item} />
+                      <span>{item.qty}</span>
+                      <AddButton item={item} />
+                    </TableCell>
+                    <TableCell className="text-right">${item.price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
         </div>
       )}
     </>
   );
 };
+function RemoveButton({ item }: { item: CartItem }) {
+  const [isPending, startTransition] = useTransition();
+  return (
+    <Button
+      disabled={isPending}
+      variant="outline"
+      type="button"
+      onClick={() =>
+        startTransition(async () => {
+          const res = await removeItemFromCart(item.productId);
+
+          if (!res.success) {
+            toast.error(res.message);
+          }
+        })
+      }
+    >
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <Minus className="w-4 h-4" />
+      )}
+    </Button>
+  );
+}
+
+function AddButton({ item }: { item: CartItem }) {
+  const [isPending, startTransition] = useTransition();
+  return (
+    <Button
+      disabled={isPending}
+      variant="outline"
+      type="button"
+      onClick={() =>
+        startTransition(async () => {
+          const res = await addItemToCart(item);
+
+          if (!res.success) {
+            toast.error(res.message);
+          }
+        })
+      }
+    >
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <Plus className="w-4 h-4" />
+      )}
+    </Button>
+  );
+}
 
 export default CartTable;
