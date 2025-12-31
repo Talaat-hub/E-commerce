5472d87152c963872c604aa03d017b54da54b189
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-31 05:15:03 +0200

    creating schema for paymeny method

---
+++ lib/validators/payment.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+import { PAYMENT_METHODS } from "../constants";
+
+// Schema for payment method
+export const paymentMethodSchema = z
+  .object({
+    type: z.string().min(1, "Payment method is required"),
+  })
+  .refine((data) => PAYMENT_METHODS.includes(data.type), {
+    path: ["type"],
+    message: "Invalid payment method",
+  });
