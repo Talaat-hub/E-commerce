bd7898987ba23b70591792304a505f7c5927e921
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2025-12-31 06:16:05 +0200

    creating order and order item types

---
+++ types/order.ts
@@ -0,0 +1,17 @@
+import {
+  insertOrderItemSchema,
+  insertOrderSchema,
+} from "@/lib/validators/order";
+import z from "zod";
+
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDelivered: boolean;
+  deliveredAt: Date | null;
+  orderitems: OrderItem[];
+  user: { name: string; email: string };
+};
