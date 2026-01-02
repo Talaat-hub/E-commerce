767d4e42f1b88f9e191da974993719ab01a4f894
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-02 13:06:12 +0200

    creating Schema for the PayPal paymentResult

--- lib/validators/payment.ts
+++ lib/validators/payment.ts
@@ -10,3 +10,11 @@ export const paymentMethodSchema = z
     path: ["type"],
     message: "Invalid payment method",
   });
+
+// Schema for the PayPal paymentResult
+export const paymentResultSchema = z.object({
+  id: z.string(),
+  status: z.string(),
+  email_address: z.string(),
+  pricePaid: z.string(),
+});
