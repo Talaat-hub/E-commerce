7cdeabe31e088d150a9fd1e409f48f599d9f2844
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-02 16:19:21 +0200

    rendering update profil component

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -2,7 +2,7 @@ import { auth } from "@/lib/auth/auth";
 import { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import { redirect } from "next/navigation";
-// import ProfileForm from "./profile-form";
+import ProfileForm from "./ProfileForm";
 
 export const metadata: Metadata = {
   title: "Customer Profile",
@@ -13,13 +13,13 @@ const Profile = async () => {
 
   if (!session?.user) {
     redirect("/sign-in");
-    }
-    
+  }
+
   return (
     <SessionProvider session={session}>
       <div className="max-w-md mx-auto space-y-4">
         <h2 className="h2-bold">Profile</h2>
-        {/* <ProfileForm /> */}
+        <ProfileForm />
       </div>
     </SessionProvider>
   );
