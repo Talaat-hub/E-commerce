4e7e4ae66d3ee67b16b2b7f7cbbe3cd278eae073
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-10 00:59:59 +0200

    rendering product carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,20 @@
+import ProductCarousel from "@/components/products/ProductCarousel/ProductCarousel";
 import ProductList from "@/components/products/ProductList/ProductList";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from "@/lib/actions/product.actions";
 
 export default async function Home() {
   const latestProducts = await getLatestProducts();
-  return <ProductList data={latestProducts} title="Newest Arrivals" />;
+  const featuredProducts = await getFeaturedProducts();
+
+  return (
+    <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
+      <ProductList data={latestProducts} title="Newest Arrivals" />
+    </>
+  );
 }
