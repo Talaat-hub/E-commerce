003bfab52407e5adfc451e6d3500155070af53bb
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-10 18:35:57 +0200

    adding reviewFormDefaultValues constant

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -46,3 +46,8 @@ export const productDefaultValues = {
 export const USER_ROLES = process.env.USERS_ROLES
   ? process.env.USERS_ROLES.split(",")
   : ["admin", "user"];
+export const reviewFormDefaultValues = {
+  title: "",
+  comment: "",
+  rating: 0,
+};
