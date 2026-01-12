836a89870b830cf2ad9a94bbcf42d225a4c44c10
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-01-12 18:30:58 +0200

    add deal count down component to main home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import ProductCarousel from "@/components/products/ProductCarousel/ProductCarousel";
 import ProductList from "@/components/products/ProductList/ProductList";
+import DealCountdown from "@/components/shared/deal-countdown/DealCountdown";
 import IconBoxes from "@/components/shared/icon-boxes/icon-boxes";
 import ViewAllProductsButton from "@/components/shared/view-all/ViewAllProudctsButton";
 import {
@@ -19,6 +20,7 @@ export default async function Home() {
       <ProductList data={latestProducts} title="Newest Arrivals" />
       <ViewAllProductsButton />
       <IconBoxes />
+      <DealCountdown />
     </>
   );
 }
