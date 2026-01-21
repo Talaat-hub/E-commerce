89ea0aa4b50dbf632f19e2095133140c62ec8294
Mahmoud Talaat <tmahmoud793@gmail.com>
2026-01-21 17:52:58 +0200

    installed JEST test environment

---
+++ components/__tests__/sample.test.js
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+
+describe('Sample Test', () => {
+    it('should pass', () => {
+        expect(true).toBe(true)
+    })
+})
